a7ab876b6bb10b77d627075c3fca65667b9c0def
sarathy1980 <sarathy1980@gmail.com>
2023-05-25 23:19:48 -0500

    init of the repo

---
+++ calculator/test_calculator.py
@@ -0,0 +1,11 @@
+import calculator
+
+class TestCalculator:
+	def test_addition(self):
+		assert 4 == calculator.add(2, 2)
+
+	def test_addition2(self):
+		assert 5 == calculator.add(2, 3)
+		
+	def test_addition3(self):
+		assert 1 == calculator.add(-2, 3)
