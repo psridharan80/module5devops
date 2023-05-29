36ce3b105ededd587766d65156fdf8ee1ae22295
sarathy1980 <sarathy1980@gmail.com>
2023-05-28 22:15:30 -0500

    fixed errors

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -2,7 +2,6 @@ import calculator
 
 
 class TestCalculator:
-
     def test_addition1(self):
         assert 4 == calculator.add(2, 2)
 
@@ -11,9 +10,9 @@ class TestCalculator:
 
     def test_addition3(self):
         assert 1 == calculator.add(-2, 3)
-        
+
     def test_subtraction4(self):
         assert -5 == calculator.subtract(-2, 3)
-        
+
     def test_multiply5(self):
-    	assert 6 == calculator.multiply(2, 3)    
+        assert 6 == calculator.multiply(2, 3)
