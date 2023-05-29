02dddfa0de075ff2401f4c0563792071c2281ff6
sarathy1980 <sarathy1980@gmail.com>
2023-05-28 22:04:15 -0500

    added tests for calculator

--- calculator/test_calculator.py
+++ calculator/test_calculator.py
@@ -3,7 +3,7 @@ import calculator
 
 class TestCalculator:
 
-    def test_addition(self):
+    def test_addition1(self):
         assert 4 == calculator.add(2, 2)
 
     def test_addition2(self):
@@ -11,3 +11,9 @@ class TestCalculator:
 
     def test_addition3(self):
         assert 1 == calculator.add(-2, 3)
+        
+    def test_subtraction4(self):
+        assert -5 == calculator.subtract(-2, 3)
+        
+    def test_multiply5(self):
+    	assert 6 == calculator.multiply(2, 3)    
